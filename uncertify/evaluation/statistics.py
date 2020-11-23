d4a2ae07194e5d528e4356ae2899ce443e6f6d2d
Matthäus Heer <matthaeusheer@gmail.com>
2020-11-24 00:19:27 +0100

    Added DoSE implementation. Need to incorporate into pipeline.

--- uncertify/evaluation/statistics.py
+++ uncertify/evaluation/statistics.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 
+from scipy.stats.kde import gaussian_kde
 from torch import nn
 from torch.utils.data import DataLoader
 import numpy as np
@@ -7,26 +8,24 @@ import pandas as pd
 
 from uncertify.evaluation.inference import yield_inference_batches, BatchInferenceResult
 
-from typing import List, Union
+from typing import List, Union, Dict
 
 
-VALID_STATISTICS = ['kl_div', 'rec_err', 'elbo']
-
-
-def get_slice_wise_statistics(model: nn.Module, data_loader: DataLoader, statistics: List[str],
-                              max_n_batches: int = None, residual_threshold: float = None) -> dict:
+def aggregate_slice_wise_statistics(model: nn.Module, data_loader: DataLoader, statistics: List[str],
+                                    max_n_batches: int = None, residual_threshold: float = None) -> dict:
     """Evaluate slice wise statistics and return aggregated results in a statistics-dict.
 
     Returns
         statistics_dict: will have a key for each statistic with a dictionary with 'all', 'healthy' and 'lesional'
                          keys with lists of slice-wise values for this statistic in this sub-dict
     """
-    assert all([item in VALID_STATISTICS for item in statistics]), f'Need to provide valid ' \
-                                                                   f'statistics ({VALID_STATISTICS})!'
-    statistics_dict = {key: list() for key in statistics + ['is_lesional', 'is_empty']}
+    assert all([item in STATISTICS_FUNCTIONS for item in statistics]), f'Need to provide valid ' \
+                                                                       f'statistics ({STATISTICS_FUNCTIONS})!'
+    statistics_dict = defaultdict(list)
     for batch_idx, batch in enumerate(yield_inference_batches(data_loader, model, max_n_batches, residual_threshold,
                                                               progress_bar_suffix=f'Infer slice statistics')):
-        statistics_dict['is_lesional'].extend(list(batch.slice_wise_is_lesional))
+        if batch.slice_wise_is_lesional is not None:
+            statistics_dict['is_lesional'].extend(list(batch.slice_wise_is_lesional))
         statistics_dict['is_empty'].extend(list(batch.slice_wise_is_empty))
         for statistic in statistics:
             statistics_dict[statistic].extend(list(get_slice_values_for_statistic(batch, statistic, 'all')))
@@ -37,30 +36,67 @@ def get_slice_values_for_statistic(batch: BatchInferenceResult, statistic: str,
                                    health_state: str = 'all') -> Union[np.ndarray]:
     """Get the slice-wise value from a batch inference result for either all, only healthy or only lesional slices."""
     assert health_state in ['all', 'healthy', 'lesional'], f'Provided health_state argument {health_state} invalid.'
+    assert statistic in STATISTICS_FUNCTIONS, f'Provided statistic not supported ' \
+                                              f'(choose from {STATISTICS_FUNCTIONS.keys()}).'
     if health_state == 'lesional':
         if batch.segmentation is None:
             raise AttributeError(f'Requested lesional slice statistics but batch result has no ground truth'
                                  f'segmentation. Thus cannot decide which slices are lesional.')
-    if statistic in ['kl_div', 'rec_err']:
-        return get_statistic_values_from_batch(batch, statistic, health_state)
-    else:
-        raise NotImplementedError(f'This statistic {statistic} has no implementation for calculations yet.')
+    keep_indices = define_health_state_indices(health_state, batch)
+    return STATISTICS_FUNCTIONS[statistic](batch)[keep_indices]
 
 
-def get_statistic_values_from_batch(batch: BatchInferenceResult, statistic: str,
-                                    health_state: str) -> np.ndarray:
-    """Straight forward query if the result is already computed during inference."""
-    assert statistic in ['kl_div', 'rec_err'], f'Provided statistic {statistic} not supported by this function.' \
-                                               f'Need to calculate it somehow else.'
+def define_health_state_indices(health_state: str, batch: BatchInferenceResult) -> np.ndarray:
+    """Numpy array holding the indices of the slices to keep based on the health state."""
     if health_state == 'all':
-        return getattr(batch, statistic)
+        return np.ones(len(batch.scan), dtype=bool)
+    elif health_state == 'healthy':
+        return np.invert(batch.slice_wise_is_lesional)
+    elif health_state == 'lesional':
+        return batch.slice_wise_is_lesional
     else:
-        if health_state == 'healthy':
-            return getattr(batch, statistic)[np.invert(batch.slice_wise_is_lesional)]
-        elif health_state == 'lesional':
-            return getattr(batch, statistic)[batch.slice_wise_is_lesional]
+        raise ValueError(f'Health state {health_state} not supported.')
 
 
-def statistics_dict_to_df(statistics_dict: dict) -> pd.DataFrame:
-    """Conversion function."""
-    statistics = list(statistics_dict.keys())
+def fit_statistics(statistics_dict: Dict[str, List[float]]) -> Dict[str, gaussian_kde]:
+    """For each set of inferred values for a given statistic, fit a KDE on it."""
+    kde_func_dict = {stat_name: None for stat_name in statistics_dict.keys()}
+
+    filtered_statistics_dict = {key: values for key, values in statistics_dict.items()
+                                if key in STATISTICS_FUNCTIONS.keys()}
+
+    for stat_name, values in filtered_statistics_dict.items():
+        kde_func_dict[stat_name] = gaussian_kde(values)
+    return kde_func_dict
+
+
+"""
+The following functions all adhere to the same function interface. They return batch-wise statistics as numpy arrays.
+"""
+
+
+def kl_div_batch_stat(batch: BatchInferenceResult) -> np.ndarray:
+    return batch.kl_div
+
+
+def rec_error_batch_stat(batch: BatchInferenceResult) -> np.ndarray:
+    return batch.rec_err
+
+
+def elbo_batch_stat(batch: BatchInferenceResult) -> np.ndarray:
+    return -batch.kl_div + batch.rec_err
+
+
+STATISTICS_FUNCTIONS = {
+    'kl_div': kl_div_batch_stat,
+    'rec_err': rec_error_batch_stat,
+    'elbo': elbo_batch_stat
+}
+
+
+def statistics_dict_to_df(statistics_dict: dict, filter_out_empty: bool = True) -> pd.DataFrame:
+    """Conversion function from statistics dict to pandas DataFrame."""
+    df = pd.DataFrame.from_dict(statistics_dict)
+    if filter_out_empty:
+        df = df.loc[np.invert(df.is_empty)]
+    return df
--- uncertify/models/vae.py
+++ uncertify/models/vae.py
@@ -7,6 +7,7 @@ from itertools import islice
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torch import nn
 import torchvision
@@ -140,18 +141,16 @@ class VariationalAutoEncoder(pl.LightningModule):
 
     def loss_function(self, reconstruction: Tensor, observation: Tensor, mask: Tensor, mu: Tensor, log_var: Tensor,
                       beta: float = 1.0, train_step: int = None):
-        mask = torch.ones_like(reconstruction) if mask is None else mask
         # p(x|z)
         if self._loss_type == 'l2':
-            p_x_z = dist.Normal(reconstruction * mask, 1.0)
+            p_x_z = dist.Normal(reconstruction, 1.0)
         elif self._loss_type == 'l1':
-            p_x_z = dist.Laplace(reconstruction * mask, 1.0)
+            p_x_z = dist.Laplace(reconstruction, 1.0)
         else:
             raise ValueError(f'Loss type "{self._loss_type}" not supported.')
 
-        slice_wise_not_empty_pixels = torch.sum(mask, dim=(1, 2, 3))
-        log_p_x_z = p_x_z.log_prob(observation * mask)
-        log_p_x_z = torch.sum(log_p_x_z, dim=(1, 2, 3)) / slice_wise_not_empty_pixels  # log likelihood for each slice
+        log_p_x_z = p_x_z.log_prob(observation)
+        log_p_x_z = torch.mean(log_p_x_z, dim=(1, 2, 3))
 
         # p(z)
         z_prior = dist.Normal(0.0, 1.0)
@@ -170,11 +169,52 @@ class VariationalAutoEncoder(pl.LightningModule):
 
         variational_lower_bound = (-mean_kl_div + mean_log_p_x_z) * self._n_m_factor
         total_loss = -variational_lower_bound
-        print()
-        print(mean_kl_div)
-        print(mean_log_p_x_z)
-        print(total_loss)
-        print('---')
+
+        return total_loss, mean_kl_div, -mean_log_p_x_z, kl_div, -log_p_x_z
+
+    def loss_function_new(self, reconstruction: Tensor, observation: Tensor, mask: Tensor, mu: Tensor, log_var: Tensor,
+                      beta: float = 1.0, train_step: int = None):
+        mask = torch.ones_like(reconstruction) if mask is None else mask
+        # p(x|z)
+        if self._loss_type == 'l2':
+            p_x_z = dist.Normal(reconstruction * mask, 1.0)
+        elif self._loss_type == 'l1':
+            p_x_z = dist.Laplace(reconstruction * mask, 1.0)
+        else:
+            raise ValueError(f'Loss type "{self._loss_type}" not supported.')
+
+        slice_wise_not_empty_pixels = torch.sum(mask, dim=(1, 2, 3))
+        log_p_x_z = p_x_z.log_prob(observation * mask)
+
+        # rec_loss_func = nn.L1Loss(reduction='none')
+        # l1_loss = rec_loss_func(observation * mask, reconstruction * mask)
+        # slice_wise_l1_loss = torch.sum(l1_loss, dim=(1, 2, 3))
+        # slice_wise_l1_loss = slice_wise_l1_loss / slice_wise_not_empty_pixels
+        # log_p_x_z = slice_wise_l1_loss
+        # log_p_x_z = torch.sum(log_p_x_z, dim=(1, 2, 3)) / slice_wise_not_empty_pixels  # log likelihood for each slice
+
+        # p(z)
+        z_prior = dist.Normal(0.0, 1.0)
+
+        # q(z|x)
+        z_post = dist.Normal(mu, torch.sqrt(torch.exp(log_var)))
+
+        # KL(q(z|x), p(z))
+        kl_div = dist.kl_divergence(z_post, z_prior)
+        kl_div = torch.mean(kl_div, dim=1)  # KL divergences for each slice
+        kl_div = self._calculate_beta(self._train_step_counter) * kl_div
+
+        # Take the mean over all batches to get batch-wise kl divergence and log likelihood compared to slice-wise
+        mean_kl_div = torch.mean(kl_div)
+        mean_log_p_x_z = torch.mean(log_p_x_z)
+
+        variational_lower_bound = (-mean_kl_div + mean_log_p_x_z) * self._n_m_factor
+        total_loss = -variational_lower_bound
+        # print()
+        # print(mean_kl_div)
+        # print(mean_log_p_x_z)
+        # print(total_loss)
+        # print('---')
         return total_loss, mean_kl_div, -mean_log_p_x_z, kl_div, -log_p_x_z
 
     def configure_optimizers(self) -> Optimizer:
