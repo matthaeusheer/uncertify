2e1525fb8c7300b88677a8af347949a6f463f00c
Matthäus Heer <matthaeusheer@gmail.com>
2020-12-06 23:17:52 +0100

    Added artificial Gauss lesions.

---
+++ uncertify/data/artificial.py
@@ -0,0 +1,52 @@
+import random
+import torch
+
+from uncertify.data.datasets import UncertifyDataset
+from uncertify.data.utils import gauss_2d_tensor_image, gaussian
+
+
+class BrainGaussBlobDataset(UncertifyDataset):
+    """
+    This dataset applies artificial high intensity Gaussian blobs to an original wrapped dataset simulating lesions.
+    """
+    def __init__(self, wrapped_dataset: UncertifyDataset) -> None:
+        super().__init__()
+        self._wrapped_dataset = wrapped_dataset
+        self._min_pixels_to_consider = 100
+        self._max_sample_tries = 100
+        self._blob_weight = 1000
+        self._std_min_max = (4, 10)
+
+    @property
+    def name(self) -> str:
+        return '_'.join([self._wrapped_dataset.name, 'gauss_blobs'])
+
+    def __len__(self) -> int:
+        return len(self._wrapped_dataset)
+
+    def __getitem__(self, idx) -> dict:
+        item_dict = self._wrapped_dataset.__getitem__(idx)
+        assert 'scan' in item_dict.keys(), f'Has no "scan" key - cannot wrap the dataset!'
+        # Add the gaussian blob onto the scan within the masked region.
+        n_pixels_within_mask = torch.sum(item_dict['mask'])
+        seg = torch.zeros_like(item_dict['scan'])
+        if n_pixels_within_mask > self._min_pixels_to_consider:
+            image = item_dict['scan'][0]
+            mask = item_dict['mask'][0]
+            height, width = image.shape
+            assert height == width, f'So far does only work on images with equal size.'
+            for _ in range(self._max_sample_tries):
+                y_idx, x_idx = torch.randint(0, min(height, width), (2,))
+                if mask[y_idx, x_idx]:
+                    x_offset = -((width // 2) - x_idx)
+                    y_offset = -((height // 2) - y_idx)
+                    std = random.randint(*self._std_min_max)
+                    gauss_blob = gauss_2d_tensor_image(grid_size=height, std=std, x_offset=x_offset, y_offset=y_offset)
+                    gauss_filter = (self._blob_weight * gauss_blob)
+                    threshold = gaussian(std, 0, std)
+                    item_dict['scan'][0] += gauss_filter * mask
+                    seg[0] = (gauss_filter > threshold) * mask
+                    break
+        item_dict['seg'] = seg
+        return item_dict
+
--- uncertify/data/utils.py
+++ uncertify/data/utils.py
@@ -1,5 +1,7 @@
+import math
 import itertools
 
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
@@ -10,3 +12,33 @@ def get_n_masked_pixels(data_loader: DataLoader, max_n_batches: int = None) -> i
     for batch in itertools.islice(data_loader, max_n_batches) if max_n_batches is not None else data_loader:
         n_masked_pixels += int(torch.sum(batch['mask']))
     return n_masked_pixels
+
+
+def gauss_2d_tensor_image(grid_size, std, y_offset=0, x_offset=0, normalize: bool = False) -> torch.tensor:
+    """Create a 2D grid of size grid_size x grid_size with a gaussian blob in it. Offsets w.r.t. image center."""
+    kernel_size = grid_size
+    # Create a x, y coordinate grid of shape (kernel_size, kernel_size, 2)
+    x_cord = torch.arange(kernel_size)
+    x_grid = x_cord.repeat(kernel_size).view(kernel_size, kernel_size)
+    y_grid = x_grid.t()
+    xy_grid = torch.stack([x_grid, y_grid], dim=-1)
+
+    mean_y = ((kernel_size - 1) + y_offset) / 2.
+    mean_x = ((kernel_size - 1) + x_offset) / 2.
+    variance = std ** 2.
+    # Calculate the 2-dimensional gaussian kernel which is
+    # the product of two gaussian distributions for two different
+    # variables (in this case called x and y)
+    gaussian_kernel = (1. / (2. * math.pi * variance)) * torch.exp(
+        -torch.sum((xy_grid - torch.tensor([mean_x, mean_y])) ** 2., dim=-1) / (2 * variance))
+    # Make sure sum of values in gaussian kernel equals 1.
+    if normalize:
+        gaussian_kernel = gaussian_kernel / torch.sum(gaussian_kernel)
+
+    # Reshape to 2d depthwise convolutional weight
+    return gaussian_kernel.view(kernel_size, kernel_size)
+
+
+def gaussian(x, mu, sig):
+    """1D Gaussian"""
+    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
